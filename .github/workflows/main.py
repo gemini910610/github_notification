23d5d83b6b88acf4ef3937462923a9bcbc80d742
unknown <gemini910610@gmail.com>
2023-02-04 20:27:56 +0800

    use requests library to send message with discord webhook

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 
 
 def load_data():
@@ -21,44 +22,32 @@ def get_channel_ids(user_ids, token):
     return channels
 
 
-def send_to_channel(channel_id, token, payload):
-    return requests.post(
-        f'https://discordapp.com/api/v6/channels/{channel_id}/messages',
-        headers={'authorization': f'Bot {token}'},
-        json=payload,
-    )
-
-
 data = load_data()
 with open('./config.json', 'r') as file:
     config = json.load(file)
-target = config['target']
-mode = config['mode']
+target = config.get('target')
+mode = config.get('mode')
 embed = {}
 
-EVENT_NAME = data['event_name']
-REPOSITORY = data['repository']
-event = data['event']
+EVENT_NAME = data.get('event_name')
+REPOSITORY = data.get('repository')
+event = data.get('event')
 
 if EVENT_NAME == 'push':
-    REF_NAME = data['ref_name']
-    if target['push'] == [] or REF_NAME in target['push']:
-        event = data['event']
-        PUSHER = event['pusher']['name']
-        head_commit = event['head_commit']
-        # HEAD_AUTHOR = head_commit['author']['username']
-        # HEAD_COMMITTER = head_commit['committer']['username']
-        # HEAD_MESSAGE = head_commit['message']
+    REF_NAME = data.get('ref_name')
+    push_target = target.get('push', [])
+    if push_target == [] or REF_NAME in push_target:
+        PUSHER = event.get('pusher').get('name')
+        head_commit = event.get('head_commit')
+        # HEAD_AUTHOR = head_commit.get('author').get('username')
+        # HEAD_COMMITTER = head_commit.get('committer').get('username')
+        # HEAD_MESSAGE = head_commit.get('message')
 
-        commits = event['commits']
+        commits = event.get('commits')
         COMMIT_COUNT = len(commits)
-        fields = [0] * COMMIT_COUNT
-        for i in range(COMMIT_COUNT):
-            commit = commits[i]
-            # author = commit['author']['username']
-            # committer = commit['committer']['username']
-            message = commit['message']
-            fields[i] = {'name': message, 'value': ''}
+        fields = [{'name': commit.get('message'), 'value': ''} for commit in commits]
+        # author = commit.get('author').get('username')
+        # committer = commit.get('committer').get('username')
 
         embed['title'] = f'[ PUSH ] {REPOSITORY}'
         embed[
@@ -68,13 +57,13 @@ if EVENT_NAME == 'push':
         embed['fields'] = fields
 
 elif EVENT_NAME == 'pull_request':
-    HEAD_REF = data['head_ref']
-    BASE_REF = data['base_ref']
-    if target['pull_request'] == [] or BASE_REF in target['pull_request']:
-        pull_request = data['event']['pull_request']
-        TITLE = pull_request['title']
-        COMMITS = pull_request['commits']
-        USER = pull_request['user']['login']
+    HEAD_REF = data.get('head_ref')
+    BASE_REF = data.get('base_ref')
+    if target.get('pull_request', []) == [] or BASE_REF in target.get('pull_request'):
+        pull_request = event.get('pull_request')
+        TITLE = pull_request.get('title')
+        COMMITS = pull_request.get('commits')
+        USER = pull_request.get('user').get('login')
         embed['title'] = f'[ PULL REQUEST ] {REPOSITORY}'
         embed[
             'description'
@@ -85,19 +74,30 @@ elif EVENT_NAME == 'pull_request':
 if embed != {}:
     match mode:
         case 'user':
-            import requests
             token = os.environ.get('token')
-            channels = get_channel_ids(config['user'], token)
+            if token is None:
+                raise Exception('you need to set discord bot token')
+            users = config.get('user', [])
+            if users == []:
+                raise Exception('you need to add user id to list')
+            channels = get_channel_ids(users, token)
             for channel in channels:
-                send_to_channel(channel, token, {'embeds': [embed]})
+                requests.post(
+                    f'https://discordapp.com/api/v6/channels/{channel}/messages',
+                    headers={'authorization': f'Bot {token}'},
+                    json={'embeds': [embed]},
+                )
         case 'channel':
-            from discordwebhook import Discord
             webhook = os.environ.get('webhook')
-            discord = Discord(url=webhook)
-            discord.post(
-                username='Github Notification',
-                avatar_url='https://avatars.githubusercontent.com/in/15368?s=64&v=4',
-                embeds=[embed]
+            if webhook is None:
+                raise Exception('you need to set discord webhook')
+            requests.post(
+                webhook,
+                json={
+                    'username': 'Github Notification',
+                    'avatar_url': 'https://avatars.githubusercontent.com/in/15368?s=64&v=4',
+                    'embeds': [embed],
+                },
             )
         case _:
             raise Exception('mode can only be user or channel')
